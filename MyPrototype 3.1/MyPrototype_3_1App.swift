e7fdff89fc9690092df8853c0b4da751b18b185c
Khulood <khulood.gmo@gmail.com>
2021-12-14 10:16:09 +0300

    Initial Commit

---
+++ MyPrototype 3.1/MyPrototype_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyPrototype_3_1App.swift
+//  MyPrototype 3.1
+//
+//  Created by Khulood  on 10/05/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyPrototype_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
